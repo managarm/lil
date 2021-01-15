928adde5de7be4901c7b8cdfd7a35bf3b5256cce
Thomas Woertman <twoertman@gmail.com>
2021-01-15 15:20:19 +0100

    Abstract GTT per gen

--- src/gtt.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "intel.h"
-
-//zero out the entire GTT
-void lil_vmem_clear(LilGpu* gpu);
-void lil_vmem_map(LilGpu* gpu, uint32_t host, GpuAddr gpu_addr, uint32_t flags);
---
+++ src/ivy_bridge/gtt.c
@@ -0,0 +1,16 @@
+#include "gtt.h"
+#include "../imports.h"
+
+void lil_ivb_vmem_clear(LilGpu* gpu) {
+    for (size_t i = 0; i < (gpu->gtt_size >> 12); i++) {
+        volatile uint32_t* gtt = (uint32_t*)(gpu->gtt_address + i * 4);
+        *gtt = 0;
+    }
+}
+
+void lil_ivb_vmem_map(LilGpu* gpu, uint64_t host, GpuAddr gpu_addr) {
+    if ((host & ~0xFFFFFFFFFF) != 0)
+        lil_panic("Ivy Bridge GPU only supports 40-bit host addresses");
+
+    *(volatile uint32_t*)(gpu->gtt_address + (gpu_addr / 0x1000) * 4) = host | (((host >> 32) & 0xFF) << 4) | 0b110 | 1; // MLC/LLC Caching, Present
+}
