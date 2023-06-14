3a005a06a858f3dd4e2630c8b44aece977ad6a33
no92 <no92.mail@gmail.com>
2023-06-14 23:28:22 +0200

    imports.h: rename free to lil_free

--- include/lil/imports.h
+++ include/lil/imports.h
@@ -19,7 +19,7 @@ __attribute__((weak)) uint32_t lil_pci_readd(void* device, uint16_t offset);
 __attribute__((weak)) void lil_sleep(uint64_t ms);
 __attribute__((weak)) void lil_usleep(uint64_t us);
 __attribute__((weak)) void* lil_malloc(size_t s);
-__attribute__((weak)) void free(void* p);
+__attribute__((weak)) void lil_free(void* p);
 __attribute__((weak)) void* lil_map(size_t loc, size_t len);
 __attribute__((weak)) void lil_log(enum LilLogType type, const char *fmt, ...);
 __attribute__((weak, noreturn)) void lil_panic(const char* msg);
