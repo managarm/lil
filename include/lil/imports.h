24e80e938fa2a3fd17a212de09e8b4a2f5d4fcaf
no92 <no92.mail@gmail.com>
2023-06-14 23:25:50 +0200

    imports.h: add lil_usleep

--- include/lil/imports.h
+++ include/lil/imports.h
@@ -17,6 +17,7 @@ __attribute__((weak)) uint16_t lil_pci_readw(void* device, uint16_t offset);
 __attribute__((weak)) void lil_pci_writed(void* device, uint16_t offset, uint32_t val);
 __attribute__((weak)) uint32_t lil_pci_readd(void* device, uint16_t offset);
 __attribute__((weak)) void lil_sleep(uint64_t ms);
+__attribute__((weak)) void lil_usleep(uint64_t us);
 __attribute__((weak)) void* lil_malloc(size_t s);
 __attribute__((weak)) void free(void* p);
 __attribute__((weak)) void* lil_map(size_t loc, size_t len);
