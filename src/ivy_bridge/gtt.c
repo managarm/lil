ad8ef093b019a6c1e5eae882ecf6545fcef1f0e8
no92 <no92.mail@gmail.com>
2023-06-19 17:36:59 +0200

    ivb/gtt: clean up GTT code

--- src/ivy_bridge/gtt.c
+++ src/ivy_bridge/gtt.c
@@ -1,11 +1,11 @@
-#include "gtt.h"
-
 #include <lil/imports.h>
+
+#include "src/gtt.h"
+#include "src/ivy_bridge/gtt.h"
 
 void lil_ivb_vmem_clear(LilGpu* gpu) {
     for (size_t i = 0; i < (gpu->gtt_size >> 12); i++) {
-        volatile uint32_t* gtt = (uint32_t*)(gpu->gtt_address + i * 4);
-        *gtt = 0;
+        GTT32_ENTRY(gpu, i << 12) = 0;
     }
 }
 
@@ -13,5 +13,5 @@ void lil_ivb_vmem_map(LilGpu* gpu, uint64_t host, GpuAddr gpu_addr) {
     if ((host & ~0xFFFFFFFFFF) != 0)
         lil_panic("Ivy Bridge GPU only supports 40-bit host addresses");
 
-    *(volatile uint32_t*)(gpu->gtt_address + (gpu_addr / 0x1000) * 4) = host | (((host >> 32) & 0xFF) << 4) | 0b110 | 1; // MLC/LLC Caching, Present
+    GTT32_ENTRY(gpu, gpu_addr) = host | (((host >> 32) & 0xFF) << 4) | GTT_IVB_CACHE_MLC_LLC | GTT_PAGE_PRESENT;
 }
